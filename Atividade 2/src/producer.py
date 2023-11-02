57e02bc21ab6cc278c4973483b8fece3942daf13
Lucas211-ctrl <lucas.vieira@sou.inteli.edu.br>
2023-11-02 00:59:31 -0300

    Update src

--- Atividade 2/src/producer.py
+++ Atividade 2/src/producer.py
@@ -1,17 +1,11 @@
 from kafka import KafkaProducer
-import json
-import random
-from time import sleep
-from datetime import datetime
 
-# Create an instance of the Kafka producer
-producer = KafkaProducer(bootstrap_servers='localhost:9092',  # Correção na porta
+producer = KafkaProducer(bootstrap_servers='localhost:9092',  
                         value_serializer=lambda v: str(v).encode('utf-8'))
 
-# Call the producer.send method with a producer-record
 try:
     while True:
-        value = input("Digite um valor para enviar ao Kafka: ")
+        value = input("Digite uma mensagem para enviar ao Kafka: ")
         producer.send('kafka-python-topic', value=value)
 except KeyboardInterrupt:
     producer.close()
